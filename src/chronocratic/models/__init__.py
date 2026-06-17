8833f57da221b908b13f80739d2ccf2f6374ed90
Wadie Skaf <wadieskaf@gmail.com>
2026-06-17 15:19:45 +0200

    feat(quick-enc-dec-contract): add HasEncoder/HasDecoder protocols and exports
    - Create contracts.py with @runtime_checkable HasEncoder and HasDecoder Protocols
    - Export HasEncoder and HasDecoder from models __init__.py
    - Sort __all__ alphabetically

--- src/chronocratic/models/__init__.py
+++ src/chronocratic/models/__init__.py
@@ -21,23 +21,26 @@ from .convolutional import (
     TSTCC,
     TSTCCModelParameters,
 )
+from .contracts import HasDecoder, HasEncoder
 from .generative import TimeVAE, TimeVAEModelParameters
 from .recurrent import TimeNet, TimeNetModelParameters
 from .transformer import TST, TSTModelParameters
 
 __all__ = [
-    "FCN",
-    "TST",
-    "TSTCC",
     "AutoTCL",
     "AutoTCLModelParameters",
     "CoST",
     "CoSTModelParameters",
+    "FCN",
+    "HasDecoder",
+    "HasEncoder",
     "MCLModelParameters",
     "Series2Vec",
     "Series2VecModelParameters",
     "TS2Vec",
     "TS2VecModelParameters",
+    "TST",
+    "TSTCC",
     "TSTCCModelParameters",
     "TSTModelParameters",
     "TimeNet",
